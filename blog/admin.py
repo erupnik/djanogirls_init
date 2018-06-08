56561993b6456272fe19d96796b31e3915e14025
ewelina rupnik <erupnik@gmail.com>
2018-06-08 17:15:31 +0200

    Add update 1

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author','title', 'created_date', 'published_date')
+    search_fields = ('title','text')
+
+#admin.site.register(Post)
